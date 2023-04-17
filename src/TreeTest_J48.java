f80c600625da898ecef97734dd1c699c12e27789
ChrisKammerer <ckamm001@outlook.com>
2023-04-17 14:10:55 -0400

    tree visualization

--- src/TreeTest_J48.java
+++ src/TreeTest_J48.java
@@ -14,8 +14,10 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.gui.treevisualizer.PlaceNode2;
 import weka.gui.treevisualizer.TreeVisualizer;
+
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Instances;
 
@@ -49,7 +51,7 @@ public class TreeTest_J48 {
             eval.evaluateModel(cls, data);
             System.out.println("Error rate Training Set: " + eval.errorRate());
             System.out.println(eval.toSummaryString());
-            double [][] confusionMatrix = eval.confusionMatrix();
+            double[][] confusionMatrix = eval.confusionMatrix();
             String out = getMatrixString(confusionMatrix);
             System.out.println("Confusion Matrix Training Set:");
             System.out.println(out);
@@ -65,14 +67,13 @@ public class TreeTest_J48 {
             out = getMatrixString(confusionMatrix);
             System.out.println(out);
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    private String getMatrixString(double [][] m) {
+    private String getMatrixString(double[][] m) {
         String out = "";
 
         for (double[] doubles : m) {
@@ -87,10 +88,10 @@ public class TreeTest_J48 {
         return out;
     }
 
-    public Instances getData(String dataPath, int classIndex){
+    public Instances getData(String dataPath, int classIndex) {
         Instances data = null;
 
-        try{
+        try {
             System.out.println("Getting Data Instances");
             // Build Instances from dataPath
             DataSource source = new DataSource(dataPath);
@@ -102,10 +103,10 @@ public class TreeTest_J48 {
         return data;
     }
 
-    public J48 getJ48Tree(String dataPath, int classIndex){
+    public J48 getJ48Tree(String dataPath, int classIndex) {
         Instances data;
         J48 cls = null;
-        try{
+        try {
             System.out.println("Building J48 Tree");
             // Get Data Set from Path
             DataSource source = new DataSource(dataPath);
@@ -115,46 +116,84 @@ public class TreeTest_J48 {
             // Build Tree
             cls = new J48();
             cls.buildClassifier(data);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return cls;
     }
 
+    public static void showTree(J48 cls) {
+
+        try {
+            String graphDescriptor = cls.graph();
+
+            final javax.swing.JFrame jf =
+                    new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+
+            jf.setSize(900, 500);
+            jf.getContentPane().setLayout(new BorderLayout());
+
+            TreeVisualizer tv = new TreeVisualizer(null, graphDescriptor, new PlaceNode2());
+
+            jf.getContentPane().add(tv, BorderLayout.CENTER);
+
+            jf.addWindowListener(new java.awt.event.WindowAdapter() {
+                public void windowClosing(java.awt.event.WindowEvent e) {
+                    jf.dispose();
+                }
+            });
+
+            jf.setVisible(true);
+            tv.fitToScreen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
         // Create Instance and Test
         TreeTest_J48 decisionTree = new TreeTest_J48();
+
 
         // Define Paths for Training and Testing Data
 //        String trainingPath = "iris_data/iris_train.csv";
 //        String testingPath = "iris_data/iris_test.csv";
 //        int classIndex = 4;
+        //output 0.0 = setosa, 1.0 = versicolor, 2.0 = virginica
+
+
+
+//        String trainingPath = "diabetes_data/diabetes_data_train.csv";
+//        String testingPath = "diabetes_data/diabetes_data_test.csv";
+//        int classIndex = 16;
 
         String trainingPath = "raisin_data/Raisin_Train.csv";
         String testingPath = "raisin_data/Raisin_Test.csv";
         int classIndex = 7;
-                // Run Tree Build and Test
+        // Run Tree Build and Test
         decisionTree.testTreeJ48(trainingPath, testingPath, classIndex);
 
         // Get Instances for Data
         Instances data = decisionTree.getData(testingPath, classIndex);
+        Instance sample = data.instance(11);
 
         // Get J48 Tree
         J48 treeClassifier = decisionTree.getJ48Tree(trainingPath, classIndex);
 
         // Test Classification for one instance
         // Instance 0
-        Instance sample = data.instance(6);
 
-        try{
+        try {
             double type = treeClassifier.classifyInstance(sample);
             double size = treeClassifier.measureTreeSize();
-            
+
             System.out.println(type);
             System.out.println(size);
-        }catch(Exception e){
+            showTree(treeClassifier);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
